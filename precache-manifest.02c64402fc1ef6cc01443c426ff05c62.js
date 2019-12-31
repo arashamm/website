41b18b317b3e802a7d8f84b7240934a67447f72a
arash_ammarlooi <arashammarlooi@gmail.com>
2019-12-31 20:05:53 +0330

    Updates

---
+++ precache-manifest.02c64402fc1ef6cc01443c426ff05c62.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "14499745a32caa69a99d",
+    "url": "/website/static/css/main.75615831.chunk.css"
+  },
+  {
+    "revision": "14499745a32caa69a99d",
+    "url": "/website/static/js/main.552de66f.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "71220fcb788a38daad1a",
+    "url": "/website/static/js/2.0e5cc028.chunk.js"
+  },
+  {
+    "revision": "5f2d349bc613161c03f1d6982c05ed51",
+    "url": "/website/static/media/HeaderSVG.5f2d349b.svg"
+  },
+  {
+    "revision": "899bf58e7b32515048dbbb34773ebfbb",
+    "url": "/website/static/media/Connections.899bf58e.svg"
+  },
+  {
+    "revision": "9df3d34a699232d12dc31acbecfcb8e3",
+    "url": "/website/static/media/FooterIcon1.9df3d34a.svg"
+  },
+  {
+    "revision": "503041ad1311bd593d25a0dcb5acd141",
+    "url": "/website/static/media/FooterIcon2.503041ad.svg"
+  },
+  {
+    "revision": "193102b6ca3d18ca2d2f72a7db8e323c",
+    "url": "/website/static/media/FooterIcon3.193102b6.svg"
+  },
+  {
+    "revision": "fa495880e3822cb323a8835edcfb02f0",
+    "url": "/website/static/media/HomeMainAppsvg.fa495880.svg"
+  },
+  {
+    "revision": "92f4af8a6a4219ae5d5caedc63f00560",
+    "url": "/website/static/media/HomeMainWebOpt.92f4af8a.svg"
+  },
+  {
+    "revision": "aa5dc371d35b303b820b62ff8892bd16",
+    "url": "/website/static/media/HomeMainOptSvg.aa5dc371.svg"
+  },
+  {
+    "revision": "11c5d7849b5ac002bf2479fa0bfccf7f",
+    "url": "/website/static/media/HomeMainGeraphic.11c5d784.svg"
+  },
+  {
+    "revision": "cc32c5b541381ec84f522c3d5d66331b",
+    "url": "/website/static/media/HomeMainGeraphicSvg.cc32c5b5.svg"
+  },
+  {
+    "revision": "c17639526ff554b741a439e8b4d18144",
+    "url": "/website/static/media/HomeMainPromo.c1763952.svg"
+  },
+  {
+    "revision": "044502b493a04eb74951ae6292f3c528",
+    "url": "/website/static/media/HomeMainPromoSvg.044502b4.svg"
+  },
+  {
+    "revision": "afa690d4157ae6b37f4a20e624e231f4",
+    "url": "/website/static/media/headerWall.afa690d4.png"
+  },
+  {
+    "revision": "2d16cb6c88978b7c71b5980ff80b9c88",
+    "url": "/website/static/media/TopMainBack.2d16cb6c.png"
+  },
+  {
+    "revision": "5422e6d5abce9c32ce251e032ea9f880",
+    "url": "/website/static/media/WebSiteSvg1.5422e6d5.svg"
+  },
+  {
+    "revision": "80149301eb57bd8778b4b90e4a68c71e",
+    "url": "/website/index.html"
+  }
+];
